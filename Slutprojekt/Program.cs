4a209fd31900496af6799fa1e8fe380ce7b8fe28
Paul Chastain <paulchast1@gmail.com>
2022-03-22 14:56:46 +0100

    wip

--- Slutprojekt/Program.cs
+++ Slutprojekt/Program.cs
@@ -1,4 +1,6 @@
-﻿using Raylib_cs;
+﻿using System.Security.AccessControl;
+using Raylib_cs;
+using System.Numerics;
 
 Raylib.SetTargetFPS(60);
 Raylib.InitWindow(1000, 1000, "Minesweeper");
@@ -17,9 +19,21 @@ void game()
         }
     }
 
+    Vector2 mousePos = Raylib.GetMousePosition();
 
-    while (!Raylib.WindowShouldClose())
-    {
+
+    while (!Raylib.WindowShouldClose()) {
+        for (int i = 0; i < Raylib.GetScreenWidth(); i += 50)
+        {
+            for (int j = 0; j < Raylib.GetScreenHeight(); j += 50 )
+            {
+                if (mousePos.X < i + 50 && mousePos.X > i && mousePos.Y + 50 < j && mousePos.Y > j && Raylib.IsMouseButtonPressed(MouseButton.MOUSE_BUTTON_LEFT))
+                {
+                    System.Console.WriteLine(mousePos);
+                }
+            }
+        }
+    
         Raylib.BeginDrawing();
         Raylib.ClearBackground(Color.GRAY);
         foreach (Square s in squares)
