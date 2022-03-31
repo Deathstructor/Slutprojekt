26df6de00080cea8ea396a1a33779554b1dcb206
Paul Chastain <paulchast1@gmail.com>
2022-03-31 19:09:01 +0200

    WIP backup

--- Slutprojekt/Squares.cs
+++ Slutprojekt/Squares.cs
@@ -21,13 +21,42 @@ public class Square
 
         if (isMine)
         {
-            Raylib.DrawCircle((int)(rec.x + (int)boxProperties.size / 2), (int)(rec.y + (int) boxProperties.size / 2), 10, Color.ORANGE);
+            Raylib.DrawCircle((int)(rec.x + (int)boxProperties.size / 2), (int)(rec.y + (int)boxProperties.size / 2), 10, Color.ORANGE);
         }
 
     }
 
     public void Click()
     {
-        
+
+    }
+
+    public void CountNeighbours()
+    {
+        if (isMine)
+        {
+            nc = -1;
+            return;
+        }
+
+        var total = 0;
+
+        for (var xo = -1; xo <= 1; xo++)
+            {
+                for (var yo = -1; yo <= 1; yo++)
+                {
+                    var i = this.i + xo;
+                    var j = this.j + yo;
+                    if (i > -1 && i < cols && j > -1 && j < rows)
+                    {
+                        var neighbor = grid[i][j];
+                        if (neighbor.mine)
+                        {
+                            total++;
+                        }
+                    }
+                }
+            }
+        this.nc = total;
     }
 }
