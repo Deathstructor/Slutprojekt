b1bd6129e2daa2d6127e850d86ae739b6330d201
Paul Chastain <paulchast1@gmail.com>
2022-04-04 20:48:29 +0200

    clicks wip

--- Slutprojekt/Squares.cs
+++ Slutprojekt/Squares.cs
@@ -9,6 +9,7 @@ enum boxProperties
 public class Square
 {
     public int nc = 0;
+    public bool clicked = false;
 
     Rectangle rec;
 
@@ -26,22 +27,16 @@ public class Square
         {
             Raylib.DrawCircle((int)(rec.x + (int)boxProperties.size / 2), (int)(rec.y + (int)boxProperties.size / 2), 10, Color.RED); // Ritar ut minorna
         }
-        else if(nc > 0) // Ritar ut siffran som anger hur många minor som finns runt den rutan
+        else if (nc > 0) // Ritar ut siffran som anger hur många minor som finns runt den rutan
         {
-            if (nc == 1) {Raylib.DrawText(nc.ToString(), (int) rec.x + 23, (int) rec.y + 15, 28, Color.DARKBLUE);}
-            if (nc == 2) {Raylib.DrawText(nc.ToString(), (int) rec.x + 18, (int) rec.y + 15, 28, Color.GREEN);}
-            if (nc == 3) {Raylib.DrawText(nc.ToString(), (int) rec.x + 18, (int) rec.y + 15, 28, Color.RED);}
-            if (nc == 4) {Raylib.DrawText(nc.ToString(), (int) rec.x + 18, (int) rec.y + 15, 28, Color.DARKPURPLE);}
-            if (nc == 5) {Raylib.DrawText(nc.ToString(), (int) rec.x + 18, (int) rec.y + 15, 28, Color.ORANGE);}
-            if (nc == 6) {Raylib.DrawText(nc.ToString(), (int) rec.x + 18, (int) rec.y + 15, 28, Color.SKYBLUE);}
-            if (nc == 7) {Raylib.DrawText(nc.ToString(), (int) rec.x + 18, (int) rec.y + 15, 28, Color.BLACK);}
-            if (nc == 8) {Raylib.DrawText(nc.ToString(), (int) rec.x + 18, (int) rec.y + 15, 28, Color.GRAY);}
+            if (nc == 1) { Raylib.DrawText(nc.ToString(), (int)rec.x + 23, (int)rec.y + 15, 28, Color.DARKBLUE); }
+            if (nc == 2) { Raylib.DrawText(nc.ToString(), (int)rec.x + 18, (int)rec.y + 15, 28, Color.GREEN); }
+            if (nc == 3) { Raylib.DrawText(nc.ToString(), (int)rec.x + 18, (int)rec.y + 15, 28, Color.RED); }
+            if (nc == 4) { Raylib.DrawText(nc.ToString(), (int)rec.x + 18, (int)rec.y + 15, 28, Color.DARKPURPLE); }
+            if (nc == 5) { Raylib.DrawText(nc.ToString(), (int)rec.x + 18, (int)rec.y + 15, 28, Color.ORANGE); }
+            if (nc == 6) { Raylib.DrawText(nc.ToString(), (int)rec.x + 18, (int)rec.y + 15, 28, Color.SKYBLUE); }
+            if (nc == 7) { Raylib.DrawText(nc.ToString(), (int)rec.x + 18, (int)rec.y + 15, 28, Color.BLACK); }
+            if (nc == 8) { Raylib.DrawText(nc.ToString(), (int)rec.x + 18, (int)rec.y + 15, 28, Color.GRAY); }
         }
-
-    }
-
-    public void Click()
-    {
-        
     }
 }
